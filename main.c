5ea7f5c812d6b5be364336e54c7af247dfc75582
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-02-20 03:00:12 +0400

    Completed path.c

--- main.c
+++ main.c
@@ -1,42 +1,113 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the simple shell program
- * @ac: Argument count
- * @av: Argument vector
- *
- * Return: 0 on success, or appropriate error code
+ * prompt - prints shell prompt
  */
-int main(int ac, char **av)
+void prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "($) ", 4);
+}
+
+/**
+ * parse_line - splits input into arguments
+ */
+char **parse_line(char *line)
+{
+	char **argv;
+	char *token;
+	int i = 0;
+
+	argv = malloc(sizeof(char *) * 64);
+	if (!argv)
+		return (NULL);
+
+	token = strtok(line, " \t\r\n");
+	while (token)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	argv[i] = NULL;
+	return (argv);
+}
+
+/**
+ * execute_cmd - executes command and returns exit status
+ */
+int execute_cmd(char **argv)
+{
+	pid_t pid;
+	int status, exit_status = 0;
+	char *full_path;
+
+	if (argv == NULL || argv[0] == NULL)
+		return (0);
+
+	full_path = _which(argv[0]);
+	if (full_path == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+		return (127);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(full_path, argv, environ) == -1)
+		{
+			perror("./hsh");
+			free(full_path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			exit_status = WEXITSTATUS(status);
+	}
+	free(full_path);
+	return (exit_status);
+}
+
+int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t nread;
-
-	(void)ac;
-	(void)av;
+	ssize_t n_read;
+	char **argv;
+	int last_status = 0, i;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
+		prompt();
+		n_read = getline(&line, &len, stdin);
+		if (n_read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
 			free(line);
-			break;
+			exit(last_status);
 		}
 
-		/* Sətir sonundakı yeni sətir simvolunu silirik */
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		/* Burada komandanı icra edəcək funksiya çağırılmalıdır */
-		/* execute_command(line); */
+		argv = parse_line(line);
+		if (argv && argv[0])
+		{
+			if (strcmp(argv[0], "exit") == 0)
+			{
+				free(argv);
+				free(line);
+				exit(last_status);
+			}
+			if (strcmp(argv[0], "env") == 0)
+			{
+				for (i = 0; environ[i]; i++)
+					printf("%s\n", environ[i]);
+				last_status = 0;
+			}
+			else
+				last_status = execute_cmd(argv);
+		}
+		free(argv);
 	}
-
-	return (0);
+	return (last_status);
 }
